febbc33448ddd94247c913c9564fa9330bff7886
Kurai <fxt.kurai@gmail.com>
2026-03-10 02:37:08 -0400

    Zombies AI
    Created the zombies AI, now they can follow the player once the player enters the detection range of the zombies

--- Fading Echos/Assets/_Scripts/Enemies/EnemyHealth.cs
+++ Fading Echos/Assets/_Scripts/Enemies/EnemyHealth.cs
@@ -1,41 +1,104 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyHealth : MonoBehaviour
 {
+    Animator anim;
+    NavMeshAgent Agent;
+
     [SerializeField] public float health;
-    RagdollManager ragdollManager;
     [SerializeField] float delayTimer = 5f;
+
+    public Transform Player;
+    public float detectionRange = 10f;
+    public float attackDistance = 3f;
+    public float attackInterval = 2f;
+
+    public bool isDead = false;
+    bool isAttacking = false;
 
     private void Start()
     {
-        ragdollManager = GetComponent<RagdollManager>();
+        Agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+
+        if (Player == null)
+        {
+            Player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+
     }
+
+    void Update()
+    {
+
+        if (isDead)
+        {
+            return;
+        }
+
+        float Distance = Vector3.Distance(transform.position, Player.position);
+
+        if (Distance <= detectionRange)
+        {
+            Agent.SetDestination(Player.position);
+            anim.SetBool("isWalking", true);
+
+            if (Distance <= attackDistance && !isAttacking)
+            {
+                StartCoroutine(PlayAttackingAnimation());
+            }
+        }
+        else
+        {
+            Agent.ResetPath();
+            anim.SetBool("isWalking", false);
+        }
+    }
+
     public void TakeDamage(float damage)
     {
         if (health > 0)
         {
+            isDead = false;
             health -= damage;
 
             if (health <= 0)
             {
                 EnemyDeath();
+                isDead = true;
             }
 
             Debug.Log("Hit");
         }
     }
-        
 
     void EnemyDeath()
     {
-        ragdollManager.TriggerRagdoll();
+        isDead = true;
+        Agent.isStopped = true;
+        anim.SetBool("isWalking", false);
+        anim.SetTrigger("dead");
         Debug.Log("Enemy Dead!");
         StartCoroutine(DespawnEnemy());
     }
 
+    IEnumerator PlayAttackingAnimation()
+    {
+        isAttacking = true;
+        Agent.isStopped = true;
+        anim.SetTrigger("Attack");
+
+        yield return new WaitForSeconds(attackInterval);
+
+        Agent.isStopped = false;
+        isAttacking = false;
+    }
+
     IEnumerator DespawnEnemy()
     {
+
         Debug.Log($"Coroutine started - waiting {delayTimer} seconds");
         yield return new WaitForSeconds(delayTimer);
         
