2a1f6a822545d81192686f1e5fdef64ecf1b9f8b
Kurai <fxt.kurai@gmail.com>
2026-04-03 17:03:37 -0400

    Win and Lose
    Added a win and lose condition without UI Elements showing it for the moment

--- Fading Echos/Assets/_Scripts/GameManager.cs
+++ Fading Echos/Assets/_Scripts/GameManager.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
 using Unity.Cinemachine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] float gameTimer = 120f;
+    [SerializeField] TextMeshProUGUI timerText;
+    float currentTime;
+
+    THealthManager healthManager;
+    bool gameEnded = false;
+
     private void Awake()
     {
         Camera.main.gameObject.AddComponent<CinemachineBrain>();
@@ -12,5 +20,52 @@ public class GameManager : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        currentTime = gameTimer;
+        healthManager = FindFirstObjectByType<THealthManager>();
+    }
+
+    private void Update()
+    {
+        if (gameEnded) return;
+
+        currentTime -= Time.deltaTime;
+
+        UpdateTimer();
+
+        if (currentTime <= 0)
+            Win();
+
+        if (healthManager != null)
+            if (healthManager.health <= 0) Lose();
+    }
+
+    public void UpdateTimer()
+    {
+        if (timerText != null)
+        {
+            int minutes = Mathf.FloorToInt(currentTime / 60);
+            int seconds = Mathf.FloorToInt(currentTime % 60);
+
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+    }
+
+    public void Win()
+    {
+        gameEnded = true;
+        Debug.Log("YOU WIN!");
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void Lose()
+    {
+        gameEnded = true;
+        Debug.Log("YOU LOST!");
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 }
